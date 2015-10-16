6432a6325915e1fb18ac0b20b7776bfbc2a80f10
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2015-10-16 19:54:42 +0900

    Support Raspberry Pi.

---
+++ fabfile.py
@@ -0,0 +1,36 @@
+#pylint: skip-file
+#
+# fabfile.py
+#
+# Copyright (c) 2015 Junpei Kawamoto
+#
+# This software is released under the MIT License.
+#
+# http://opensource.org/licenses/mit-license.php
+#
+from fabric.api import *
+from fabric.contrib.project import rsync_project
+env.use_ssh_config = True
+
+PACKAGE = "ambassadors"
+
+@task
+def build():
+    """ Build a Docker image.
+    """
+    run("mkdir -p " + PACKAGE)
+    rsync_project(
+        local_dir=".", remote_dir=PACKAGE, exclude=['.git'])
+    with cd(PACKAGE):
+        run("docker build -t jkawamoto/{0} .".format(PACKAGE))
+
+@task
+def rpi_build():
+    """ Build a Docker image for Raspberry Pi.
+    """
+    run("mkdir -p " + PACKAGE)
+    rsync_project(
+        local_dir=".", remote_dir=PACKAGE, exclude=['.git'])
+    with cd(PACKAGE):
+        run("mv Dockerfile.rpi Dockerfile")
+        run("docker build -t jkawamoto/rpi-{0} .".format(PACKAGE))
